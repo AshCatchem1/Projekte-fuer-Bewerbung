83336c83cbaea58865e72c270743cf80b9a1d517
Andrucha2023 <Andrucha2023@ANDREJS-MSI-NB>
2023-11-30 16:24:15 +0100

    Projektdateien hinzufügen.

---
+++ Mentalo(Magic8Ball)/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Mentalo_Magic8Ball_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string[] answers = { "Ja", "Nein", "Vielleicht", "Frag später nochmal", "Wahrscheinlich nicht", "Auf jeden Fall" };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //TODO: Methode erstellen die auch auf Enter-Taste reagiert
+        private void Image_Click(object sender, MouseButtonEventArgs e)
+        {
+            // Benutzerfrage erhalten
+            string userQuestion = txt_UserEingabe.Text;
+
+            // Überprüfen, ob eine gültige Frage eingegeben wurde
+            if (string.IsNullOrWhiteSpace(userQuestion) || userQuestion.ToLower() == "frage eingeben!")
+            {
+                // Wenn keine gültige Frage eingegeben wurde, zeige einen Hinweis an
+                outputLabel.Content = "Bitte eine gültige Frage eingeben!";
+            }
+            else
+            {
+                // Zufällige Antwort auswählen
+                Random random = new Random();
+                int index = random.Next(answers.Length);
+                string answer = answers[index];
+
+                // Antwort anzeigen
+                outputLabel.Content = $"Frage: {userQuestion}\nAntwort: {answer}";
+            }
+        }
+
+
+        private void txt_UserEingabe_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // Textbox leeren, wenn der Benutzer den Fokus erhält
+            txt_UserEingabe.Text = "";
+        }
+    }
+}
---
+++ P01_SQL_Datenbankanbindung_Konsole/Program.cs
@@ -0,0 +1,227 @@
+﻿//namespace P01_SQL_Datenbankanbindung_Konsole
+//{
+//    using System;
+//    using Microsoft.Data.Sqlite;
+
+//    internal class Program
+//    {
+//        static void Main()
+//        {
+//            try
+//            {
+//                // Verbindung zur SQLite-Datenbank herstellen
+//                using (var connection = new SqliteConnection("Data Source=sample.db"))
+//                {
+//                    connection.Open();
+
+//                    // Tabelle erstellen (wenn nicht vorhanden)
+//                    using (var command = connection.CreateCommand())
+//                    {
+//                        // SQL-Befehl zum Erstellen d. Tabelle Persons
+//                        command.CommandText = "CREATE TABLE IF NOT EXISTS Persons (Id INTEGER PRIMARY KEY, Name TEXT)";
+//                        command.ExecuteNonQuery();
+//                    }
+
+//                    // Check für Doppeldaten
+//                    using (var command = connection.CreateCommand())
+//                    {
+//                        command.CommandText = "SELECT COUNT(*) FROM Persons";
+//                        int count = Convert.ToInt32(command.ExecuteScalar());
+
+//                        // Datenimplementierung
+//                        if (count == 0)
+//                        {
+//                            InsertData(connection, "John Doe");
+//                            InsertData(connection, "Jane Smith");
+//                            InsertData(connection, "Max Mustermann");
+//                            InsertData(connection, "Thomas A. Anderson");
+//                        }
+//                    }
+
+//                    // Daten abrufen und ausgeben
+//                    Console.WriteLine("Personen in der Datenbank:");
+//                    ReadData(connection);
+//                }
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine($"Fehler: {ex.Message}");
+//            }
+//        }
+
+//        static void InsertData(SqliteConnection connection, string name)
+//        {
+//            using (var command = connection.CreateCommand())
+//            {
+//                // SQL-Befehl zum Einfügen von Daten in die Tabelle Persons
+//                command.CommandText = "INSERT INTO Persons (Name) VALUES (@Name)";
+//                command.Parameters.AddWithValue("@Name", name);
+//                command.ExecuteNonQuery();
+//            }
+//        }
+
+//        static void ReadData(SqliteConnection connection)
+//        {
+//            using (var command = connection.CreateCommand())
+//            {
+//                // SQL-Befehl zum Auswählen aller Daten aus der Tabelle Persons
+//                command.CommandText = "SELECT * FROM Persons";
+
+//                using (var reader = command.ExecuteReader())
+//                {
+//                    while (reader.Read())
+//                    {
+//                        // Daten aus der Ergebnismenge abrufen und ausgeben
+//                        Console.WriteLine($"Id: {reader.GetInt32(0)}, Name: {reader.GetString(1)}");
+//                    }
+//                }
+//            }
+//            Console.WriteLine($"Aktuelles Arbeitsverzeichnis: {Environment.CurrentDirectory}");
+//        }
+//    }
+//}
+
+using Microsoft.Data.Sqlite;
+using System;
+
+namespace P01_SQL_Datenbankanbindung_Konsole
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                string connectionString = "Data Source=sample.db";
+
+                // Verbindung zur SQLite-Datenbank herstellen
+                using (var connection = new SqliteConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Tabelle erstellen (wenn nicht vorhanden)
+                    using (var command = connection.CreateCommand())
+                    {
+                        // SQL-Befehl zum Erstellen der Tabelle Persons
+                        command.CommandText = "CREATE TABLE IF NOT EXISTS Persons (Id INTEGER PRIMARY KEY, Name TEXT)";
+                        command.ExecuteNonQuery();
+                    }
+
+                    while (true)
+                    {
+                        Console.Clear(); // Bildschirm leeren
+
+                        // Menü anzeigen
+                        Console.WriteLine("1. Daten lesen");
+                        Console.WriteLine("2. Daten hinzufügen");
+                        Console.WriteLine("3. Daten löschen");
+                        Console.WriteLine("4. Beenden");
+
+                        // Benutzereingabe lesen
+                        Console.Write("Auswahl: ");
+                        string input = Console.ReadLine();
+
+                        // Aktion basierend auf Benutzereingabe auswählen
+                        switch (input)
+                        {
+                            case "1":
+                                Console.Clear();
+                                Console.WriteLine("Daten lesen:");
+                                ReadData(connection);
+                                break;
+
+                            case "2":
+                                Console.Clear();
+                                Console.WriteLine("Daten hinzufügen:");
+                                Console.Write("Name eingeben: ");
+                                string nameToAdd = Console.ReadLine();
+                                InsertData(connection, nameToAdd);
+                                break;
+
+                            case "3":
+                                Console.Clear();
+                                Console.WriteLine("Daten löschen:");
+                                Console.Write("ID zum Löschen eingeben: ");
+                                if (int.TryParse(Console.ReadLine(), out int idToDelete))
+                                {
+                                    DeleteData(connection, idToDelete);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Ungültige Eingabe. ID muss eine ganze Zahl sein.");
+                                }
+                                break;
+
+                            case "4":
+                                Console.WriteLine("Programm wird beendet.");
+                                return;
+
+                            default:
+                                Console.WriteLine("Ungültige Auswahl. Bitte wählen Sie 1, 2, 3 oder 4.");
+                                break;
+                        }
+
+                        // Warte darauf, dass der Benutzer eine Taste drückt, um zum Menü zurückzukehren
+                        Console.WriteLine("\nDrücken Sie eine beliebige Taste, um fortzufahren...");
+                        Console.ReadKey();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Fehler: {ex.Message}");
+            }
+        }
+
+        static void InsertData(SqliteConnection connection, string name)
+        {
+            using (var command = connection.CreateCommand())
+            {
+                // SQL-Befehl zum Einfügen von Daten in die Tabelle Persons
+                command.CommandText = "INSERT INTO Persons (Name) VALUES (@Name)";
+                command.Parameters.AddWithValue("@Name", name); // Parameter hinzufügen
+                command.ExecuteNonQuery(); // Ausführen des Befehls
+                Console.WriteLine("Daten wurden hinzugefügt.");
+            }
+        }
+
+        static void DeleteData(SqliteConnection connection, int id)
+        {
+            using (var command = connection.CreateCommand())
+            {
+                // SQL-Befehl zum Löschen von Daten aus der Tabelle Persons
+                command.CommandText = "DELETE FROM Persons WHERE Id = @Id";
+                command.Parameters.AddWithValue("@Id", id); // Parameter hinzufügen
+                int rowsAffected = command.ExecuteNonQuery(); // Ausführen des Befehls
+                if (rowsAffected > 0)
+                {
+                    Console.WriteLine("Daten wurden gelöscht.");
+                }
+                else
+                {
+                    Console.WriteLine("Keine Daten gefunden, die gelöscht werden können.");
+                }
+            }
+        }
+
+        static void ReadData(SqliteConnection connection)
+        {
+            using (var command = connection.CreateCommand())
+            {
+                // SQL-Befehl zum Auswählen aller Daten aus der Tabelle Persons
+                command.CommandText = "SELECT * FROM Persons";
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        // Daten aus der Ergebnismenge abrufen und ausgeben
+                        Console.WriteLine($"Id: {reader.GetInt32(0)}, Name: {reader.GetString(1)}");
+                    }
+                }
+            }
+        }
+    }
+}
+
+
